e1dcbc0119dce99bde8cef72b89623eb6a6537a2
Rifad Lafir <rifadahmedlafir@gmail.com>
2017-06-30 19:17:21 +0300

    added About component

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import '../styles/about.css';
+
+class About extends Component{
+  render(){
+    return(
+      <div className="about">
+
+
+      </div>
+    )
+  }
+}
+
+export default About;
