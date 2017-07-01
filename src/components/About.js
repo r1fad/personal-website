9a7abcee80a0254e573a522aa21526b5cf28483b
Rifad Lafir <rifadahmedlafir@gmail.com>
2017-07-01 15:30:52 +0300

    removed firebase hosting

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,13 @@ class About extends Component{
   render(){
     return(
       <div className="about">
-
+        <h2 className="heading">About Me</h2>
+        <center>
+          <p className="sub-text">
+            I am a second year student studying for a <a href="http://www.cs.manchester.ac.uk/study/undergraduate/courses/computer-science/?code=09616&pg=home">
+            MEng in Computer Science</a> at <a href="http://www.manchester.ac.uk/">The University of Manchester</a>
+          </p>
+        </center>
 
       </div>
     )
