d0a75bae45c5338b3a8e942fbd61fc1437e0093f
Rifad Lafir <rifadahmedlafir@gmail.com>
2017-07-05 21:42:51 +0300

    Added experience component

---
+++ src/components/Experience.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import ScrollableAnchor from 'react-scrollable-anchor';
+import '../styles/experience.css';
+
+class Experience extends Component{
+  render(){
+    return(
+      <ScrollableAnchor id={"section-experience"}>
+        <div className="experience">
+
+        </div>
+      </ScrollableAnchor>
+    )
+  }
+}
+
+export default Experience;
