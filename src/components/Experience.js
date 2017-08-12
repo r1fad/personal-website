f43e6b19b64edb4a0180fc3ce19283648b5fcb3a
Rifad Lafir <rifadahmedlafir@gmail.com>
2017-08-12 15:21:21 +0300

    redesigned experience page

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 import ScrollableAnchor from 'react-scrollable-anchor';
-import {Card, CardTitle, CardText, CardBlock,
-        CardSubtitle, Col, CardFooter} from 'reactstrap';
 import '../styles/experience.css';
 
 class Experience extends Component{
@@ -10,44 +8,35 @@ class Experience extends Component{
       <ScrollableAnchor id={"section-experience"}>
         <div className="experience">
           <h2 className="headingE">Experience</h2>
-          <Col sm="11" className="exp-cards">
 
-            <Card>
-              <CardBlock>
-                <CardTitle>
-                  Computer Science Society,
-                  The University of Manchester
-                </CardTitle>
-                <CardSubtitle>Co-Chair</CardSubtitle>
-                <CardText>
-                  In charge of chairing society meetings and
-                  organizing social and educational events with
-                  the help of other members for students in the
-                  Computer Science department at the University.
-                </CardText>
-              </CardBlock>
-              <CardFooter>Since May, 2017</CardFooter>
-            </Card>
+          <div className="exp-details">
+            <div className="exp">
+              <h3>Computer Science Society(CSSoc)</h3>
+              <h4>President</h4>
+              <h6>Since May 2017</h6>
+              <p>
+                In charge of chairing society meetings and
+                organizing social and educational events with
+                the help of other members for students in the
+                Computer Science department at the University
+                of Manchester.
+              </p>
+            </div>
 
-            <Card>
-              <CardBlock>
-                <CardTitle>
-                  School of Computer Science,
-                  The University of Manchester
-                </CardTitle>
-                <CardSubtitle>
-                  First Year Student Representative
-                </CardSubtitle>
-                <CardText>
-                  Served as a liaison between the student
-                  community at The University of Manchester’s
-                  CS School and the staff members
-                </CardText>
-              </CardBlock>
-              <CardFooter> October, 2016 - May, 2017 </CardFooter>
-            </Card>
+            <div className="exp">
+              <h3>School of Computer Science,
+              The University of Manchester</h3>
+              <h5>First Year Student Representative</h5>
+              <h6>October 2016 - May 2017</h6>
+              <p>
+                Served as a liaison between the student
+                community at The University of Manchester’s
+                CS School and the staff members.
+              </p>
+            </div>
 
-          </Col>
+          </div>
+
         </div>
       </ScrollableAnchor>
     )
