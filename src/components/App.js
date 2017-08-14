dee11721a0465ab5755850733c9ff1916eb9a5fa
Rifad Lafir <rifadahmedlafir@gmail.com>
2017-08-14 18:36:11 +0300

    added proper analytics

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,19 @@ import Home from './Home';
 import NavBar from './NavBar';
 import About from './About';
 import Experience from './Experience';
+import ReactGA from 'react-ga';
 
 
 class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      someData: null,
+    };
+
+    ReactGA.initialize('UA-93024360-2');
+    ReactGA.pageview(window.location.pathname);
+  }
   render(){
     return(
       <div>
